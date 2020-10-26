2147f0cd3016946e569adfa0753528e26e00a9a1
rohith-s-p <64318022+rohith-s-p@users.noreply.github.com>
2020-10-27 01:30:50 +0530

    update contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../styling/contact.css";
-import Typing from "react-typing-animation";
+import ContactForm from "./contactForm";
 
 class Contact extends Component {
   render() {
@@ -8,16 +8,8 @@ class Contact extends Component {
       <div>
         <div className="content">
           <div className="container h-100">
-            <div className="row align-items-center h-100">
-              <div className="col offset-md-2">
-                <Typing speed={50} cursorClassName="cursor">
-                  <div className="text">
-                    <span>Contact - Yet to be updated.</span>
-                    <Typing.Delay ms={99999999} />
-                  </div>
-                </Typing>
-              </div>
-            </div>
+            <br /> <br /> <br />
+            <ContactForm />
           </div>
         </div>
       </div>
---
+++ src/components/contactForm.jsx
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+import "../styling/contact.css";
+import Typing from "react-typing-animation";
+
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      displayed: false,
+      emailSent: null,
+    };
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ disabled: true, emailSent: true });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col">
+            <Typing speed={50} cursorClassName="cursor">
+              <div className="text">
+                <span>Contact Me </span>
+                <Typing.Delay ms={99999999} />
+              </div>
+            </Typing>
+          </div>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  name="name"
+                  placeholder="Name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  name="subject"
+                  placeholder="Subject"
+                  value={this.state.subject}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-group">
+                <textarea
+                  className="form-control"
+                  name="message"
+                  placeholder="Message"
+                  rows="5"
+                  value={this.state.message}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col" style={{ textAlign: "right" }}>
+              <button
+                type="submit"
+                className="btn btn-outline-info"
+                disabled={this.state.disabled}
+              >
+                Send
+              </button>
+            </div>
+          </div>
+
+          {this.state.emailSent === true && (
+            <div className="row">
+              <div
+                className="col-md-6 offset-md-3"
+                style={{ fontSize: "large", textAlign: "center" }}
+              >
+                <p className="alert alert-success" role="alert">
+                  Email Sent!
+                </p>
+              </div>
+            </div>
+          )}
+          {this.state.emailSent === false && (
+            <div className="row">
+              <div
+                className="col-md-6 offset-md-3"
+                style={{ textAlign: "center" }}
+              >
+                <p className="alert alert-danger" role="alert">
+                  Email Not Sent!
+                </p>
+              </div>
+            </div>
+          )}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
