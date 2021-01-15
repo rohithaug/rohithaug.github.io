d38abb3eadd0add090d6f1c73c09f0c2adeacf08
rohith-s-p <64318022+rohith-s-p@users.noreply.github.com>
2021-01-15 22:27:53 +0530

    Update About Page

--- src/components/about/textContent.jsx
+++ src/components/about/textContent.jsx
@@ -7,13 +7,13 @@ class TextContent extends Component {
       <div>
         <div className="text-content">
           Hello! I'm Rohith, based in Tamil Nadu, India. I'm an Engineering
-          aspirant interested in the field of Software and Automation and highly
+          aspirant interested in the field of Software, AI and Automation and highly
           passionate about Math, Robotics, Analytics and Management. A
           Philanthropist, which I do feel the most important aspect of my life.
         </div>
         <br />
         <div className="text-content">
-          I am currently working as an Analyst Programmer at{" "}
+          I am currently working as a Software Developer at{" "}
           <a
             href="https://www.ramco.com/"
             rel="noopener noreferrer"
