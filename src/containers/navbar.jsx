4326d575bc334ba22774dac243342cd649be806e
rohith <rohithaug@gmail.com>
2022-11-11 18:40:14 -0800

    navbar update for smaller screens

--- src/containers/navbar.jsx
+++ src/containers/navbar.jsx
@@ -5,6 +5,10 @@ import { NavLink } from "react-router-dom";
 // IMPORT STYLES
 import "../styles/navbar.css";
 
+// IMPORT ICONS
+import MenuIcon from "../assets/icons/menu";
+import CancelIcon from "../assets/icons/cancel";
+
 const Navbar = () => {
   const [click, setClick] = React.useState(false);
 
@@ -18,12 +22,12 @@ const Navbar = () => {
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
             ROHITH S P
-            <i className="fa fa-code"></i>
           </NavLink>
 
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
               <NavLink
+                exact
                 to="/"
                 className="nav-links"
                 onClick={click ? handleClick : null}
@@ -60,13 +64,16 @@ const Navbar = () => {
                 className="nav-links"
                 onClick={click ? handleClick : null}
               >
-                Contact Me
+                Contact
               </NavLink>
             </li>
           </ul>
 
           <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+            {click
+              ? <CancelIcon /> 
+              : <MenuIcon />
+            }
           </div>
         </div>
       </nav>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,19 +9,30 @@ const About = () => {
     <div className="about-container">
       <div className="about-intro">
         <p className="about-details">
-          Hi there! I am a computer science and engineering aspirant interested in the field of Software Development, AI & ML, and Automation and highly passionate about Math, Analytics and Management.
+          Hi there! I am a computer science and engineering aspirant interested in the field of Software Development, AI & ML, 
+          and Automation and highly passionate about Math, Analytics and Management.
         </p>
         <p className="about-details">
-          Having done my undergrad in Robotics and Automation, I developed a keen interest on building softwares and Machine Learning for various applications, including Robots, Industrial Softwares, Autonomous Cars and understood that the application for software and ML is wide to be applied in various industries. After my undergrad, I worked for 2 years as an Analyst Programmer in the New Initiatives team of Ramco Systems, where I got the opportunity to work with the leadership team and work on interesting and fast paced products that directly impact the customers and various internal teams. I had the opportunity to build a full stack system from scratch with guidance from the stakeholders, which was released and is currently in live with 50+ customers. Most of my work were in Full Stack Development, Integration with JIRA, Distributed Systems, Machine Learning and Deployment Automation.
+          Having done my undergrad in Robotics and Automation, I developed a keen interest on building softwares and Machine Learning 
+          for various applications, including Robots, Industrial Softwares, Autonomous Cars and understood that the application 
+          for software and ML is wide to be applied in various industries. After my undergrad, I worked for 2 years as an 
+          Analyst Programmer in the New Initiatives team of Ramco Systems, where I got the opportunity to work with the 
+          leadership team and develop interesting and fast paced products that directly impact the customers and various internal teams. 
+          I had the opportunity to build a full stack system from scratch with guidance from the stakeholders, which was released 
+          and is currently in live with 50+ customers. Most of my work were in Full Stack Development, Integration with JIRA, 
+          Distributed Systems, Machine Learning and Deployment Automation.
         </p>
         <p className="about-details">
-          I'm currently looking for an Internship Role for Summer 2023. I believe that with my general problem solving and math interest and fundamental knowledge of computer science, I would be able to adapt and work on any projects that suits my interest and background and the goal and corporate mission of the company.
+          I'm currently looking for an Internship Role for Summer 2023. I believe that with my general problem solving and math interest 
+          and fundamental knowledge of computer science, I would be able to adapt and work on any projects that suits my interest and 
+          background and the goal and corporate mission of the company.
         </p>
 
         <hr />
 
         <p className="about-quote">
-          "And when the account of the ages is etched into the cosmos, let those who exist long after us know that this was our finest hour!" - Optimus Prime
+          "And when the account of the ages is etched into the cosmos, let those who exist long after us know that this was our 
+          finest hour!" - Optimus Prime
         </p>
       </div>
     </div>
