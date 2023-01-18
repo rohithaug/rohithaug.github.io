7bb867133d57de449bf8649dce4e62d23acc8c28
rohith <rohithaug@gmail.com>
2023-01-17 23:56:28 -0800

    update about me content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,29 +9,13 @@ const About = () => {
     <div className="about-container">
       <div className="about-intro">
         <p className="about-details">
-          As a computer science and engineering aspirant, I am deeply interested in the field of software development, 
-          AI, and machine learning. My passion for math, analytics, and management led me to pursue an undergraduate degree 
-          in Robotics and Automation, where I developed a keen interest in building software and machine learning for various 
-          applications. From robots and industrial software to autonomous cars, I quickly realized the vast potential of software 
-          and ML in various industries.
+            As a computer science and engineering professional, I am deeply passionate about the intersection of software development, artificial intelligence, and machine learning. My interest in these fields stems from my background in math, analytics, and management, which led me to pursue an undergraduate degree in Robotics and Automation. Through this program, I developed a keen interest in building software and machine learning for various applications, from robots and industrial software to autonomous cars. I quickly realized the vast potential of software and machine learning in various industries, and knew that I wanted to pursue a career in this field.
         </p>
         <p className="about-details">
-          After completing my undergraduate studies, I spent two years working as an Analyst Programmer in the 
-          New Initiatives team at Ramco Systems. This role gave me the opportunity to work closely with the 
-          leadership team and develop interesting, fast-paced products that had a direct impact on customers 
-          and internal teams. One of my most notable achievements in this role was building a full stack system 
-          from scratch with guidance from stakeholders, which was eventually released and is currently being 
-          used by over 60 customers.
+          After completing my undergraduate studies, I spent two years working as an Analyst Programmer in the New Initiatives team at Ramco Systems. This role gave me the opportunity to work closely with the leadership team and develop interesting, fast-paced products that had a direct impact on customers and internal teams. One of my most notable achievements in this role was building a full stack system from scratch, with guidance from stakeholders, which was eventually released and is currently being used by over 60 customers.
         </p>
         <p className="about-details">
-          I specialize in full stack development and machine learning. My technical skills are complemented 
-          by my ability to think creatively, analyze problems, and communicate effectively. Currently, I am 
-          pursuing a Master of Science in Computer Science from the University of California San Diego, with 
-          a focus in software engineering and artificial intelligence. I am seeking an internship opportunity 
-          for Summer 2023, and I am confident that my general problem-solving skills, math background, 
-          and fundamental knowledge of computer science will allow me to adapt and excel in any project 
-          that aligns with my interests and background. With my skills and passion, I am excited to make 
-          a positive impact in any organization and work towards achieving their goals and corporate mission.
+          I specialize in full stack product development and machine learning. My technical skills are complemented by my ability to think creatively, analyze problems, and communicate effectively. Currently, I am pursuing a Master of Science in Computer Science from the University of California, San Diego, with a focus in software engineering and artificial intelligence. I am seeking an internship opportunity for Summer 2023, and I am confident that my general problem-solving skills, math background, and fundamental knowledge of computer science will allow me to adapt and excel in any project that aligns with my interests and background. With my skills and passion, I am excited to make a positive impact in any organization and work towards achieving their goals and corporate mission.
         </p>
 
         <hr />
